5ebebefb2cce7f444c35bed4cd887cf0ad81935c
Bailey Mejia <baileymejia28@gmail.com>
2023-01-18 14:46:22 -0800

    more changes.

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -5,29 +5,29 @@ const router = require('express').Router();
 
 //Route the file path to api/notes
 router.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/db/db.json'));
+    res.sendFile(path.join(__dirname, './db.json'));
 });
 
 router.post('/api/notes', (req, res) => {
     let newNote = req.body;
-    let noteList = JSON.parse(fs.readFileSync('../db/db.json', 'utf8'));
+    let noteList = JSON.parse(fs.readFileSync('./db.json', 'utf8'));
     let noteLength = (noteList.length).toString();
 
     newNote.id = noteLength;
     noteList.push(newNote);
 
-    fs.writeFileSync("../db/db.json", JSON.stringify(noteList));
+    fs.writeFileSync("/db.json", JSON.stringify(noteList));
     res.json(noteList);
 });
 
 router.delete('/api/notes/:id', (req, res) => {
-    let noteList = JSON.parse(fs.readFileSync('../db/db.json', 'utf8'));
+    let noteList = JSON.parse(fs.readFileSync('/db.json', 'utf8'));
     let noteId = (req.params.id).toString();
     noteList = noteList.filter(selected => {
         return selected.id != noteId;
     });
 
-    fs.writeFileSync('../db/db.json', JSON.stringify(noteList));
+    fs.writeFileSync('/db.json', JSON.stringify(noteList));
     res.json(noteList);
 });
 
--- Develop/server.js
+++ Develop/server.js
@@ -11,10 +11,10 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 //Set up express for data parse
+app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.use(express.static("public"));
 app.use('/', htmlRoutes);
 app.use('/', apiRoutes);
 
