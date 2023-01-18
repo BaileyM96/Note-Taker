da0a0bddc610b2eb8da25154aa97a8546b0925fa
Bailey Mejia <baileymejia@Baileys-MacBook-Pro.local>
2023-01-18 12:52:35 -0800

    need to refactor code to make get notes to appear.

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -3,8 +3,16 @@ const path = require('path');
 //This creates a router and uses express
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
-})
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+//Use wildcard to access everything that follows the base URL
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public.index.html'));
+});
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
-// Came up with routes with tutor to link the page to the main index file
-const htmlRoutes = require('./routes/htmlRoutes');
-const apiRoutes = require('./routes/api');
+
+
+
 
 // Set up port 3001
 const PORT = process.env.PORT || 3001;
@@ -15,8 +13,8 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use(express.static("public"));
-app.use('/', htmlRoutes);
-app.use('/', apiRoutes);
+
+
 
 
 
