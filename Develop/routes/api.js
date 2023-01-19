3aaea1d9f9bad2cc8a369c635800a82cb4d10c19
Bailey Mejia <baileymejia@Baileys-MBP.lan>
2023-01-19 14:37:48 -0800

    fixed api routes still getting error.

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,27 +1,26 @@
-//Set up the api routes
-//load the path module to work withh files and directory
+//Set up the dependencies
 const path = require('path');
 const router = require('express').Router();
 const fs = require('fs');
 
 
 
-//Route the file path to api/notes
-router.get('/notes', (req, res) => {
+//Route the file path to api/notes return all notes as JSON
+router.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../db/db.json'));
 });
 
-router.post('/api/notes', (req, res) => {
-    let dataBase = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
+router.post('../api/notes', (req, res) => {
+    let dataBase = fs.readFileSync('/db/db.json');
     dataBase = JSON.parse(dataBase);
-    res.json(dataBase);
+    // res.json(dataBase);
 
     let userNote = {
         title: req.body.title,
-        text: req.body.text
+        text: req.body.text,
     };
     dataBase.push(userNote);
-    fs.writeFileSync('db/db.json', JSON.stringify(dataBase));
+    fs.writeFileSync('/db/db.json', JSON.stringify(dataBase));
     res.json(dataBase);
     // let newNote = req.body;
     // let noteList = JSON.parse(fs.readFileSync('../db/db.json', 'utf8'));
@@ -34,16 +33,16 @@ router.post('/api/notes', (req, res) => {
     // res.json(noteList);
 });
 
-router.delete('/api/notes/:id', (req, res) => {
-    let noteList = JSON.parse(fs.readFileSync('../db/db.json', 'utf8'));
-    let noteId = (req.params.id).toString();
-    noteList = noteList.filter(selected => {
-        return selected.id != noteId;
-    });
+// router.delete('/api/notes/:id', (req, res) => {
+//     let noteList = JSON.parse(fs.readFileSync('../db/db.json', 'utf8'));
+//     let noteId = (req.params.id).toString();
+//     noteList = noteList.filter(selected => {
+//         return selected.id != noteId;
+//     });
 
-    fs.writeFileSync('../db/db.json', JSON.stringify(noteList));
-    res.json(noteList);
-});
+//     fs.writeFileSync('../db/db.json', JSON.stringify(noteList));
+//     res.json(noteList);
+// });
 
 
 
